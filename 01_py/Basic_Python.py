9e37c7a7c73ea84879428f2936f95d5c24e75275
Jetson User Group For Korean <59607996+jetsonworld@users.noreply.github.com>
2020-01-27 23:45:04 +0900

    Add files via upload

---
+++ 01_py/Basic_Python.py
@@ -0,0 +1,15 @@
+import os
+
+a = 8
+b = 10
+c = a * b
+
+print('-------------------')
+print('a = 8')
+print('b = 10')
+print('a * b = ',c)
+print('-------------------')
+os.system('python -V')
+os.system('python2 -V')
+os.system('python3 -V')
+print('-------------------')
